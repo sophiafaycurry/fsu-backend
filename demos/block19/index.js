23b5def85e510f1661968bf3e73347397f6d16f6
Mark Lawrence <mark.lawrence@fullstackacademy.com>
2025-04-07 14:20:40 -0400

    add block19 demo

---
+++ demos/block19/index.js
@@ -0,0 +1,19 @@
+// === State ===
+// Here, we define variables for the data that our program needs to remember.
+// We call this data "state" because it represents the state of our program.
+// This is also where we define functions to modify the state.
+
+/** Adds a shape with random properties to the `shapes` array */
+
+// === Render ===
+// To "render" is to update the DOM to reflect the current state.
+// In this section, we define the functions to render state.
+
+/** Updates the DOM to reflect the current state. */
+
+// === Script ===
+// In this section, we call the functions that we've defined above.
+
+// `setInterval` will call the callback function every 1000 milliseconds (1 second)
+// and return an interval ID that we can use to stop the interval later.
+// Calling `clearInterval(addShapeIntervalId)` will stop the interval.
