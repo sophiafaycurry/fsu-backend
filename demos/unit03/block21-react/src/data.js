456f4de2478deb500fe6339bba35054f78637ffd
Mark Lawrence <mark.lawrence@fullstackacademy.com>
2025-04-28 17:40:49 -0400

    update demos

---
+++ demos/unit03/block21-react/src/data.js
@@ -0,0 +1,5 @@
+export const dogList = [
+  { id: 1, name: "Logan", breed: "Italian Greyhound", age: 14 },
+  { id: 2, name: "Chase", breed: "Italian Greyhound", age: 12 },
+  { id: 3, name: "Lincoln", breed: "Mixed Rescue", age: 6 },
+];
